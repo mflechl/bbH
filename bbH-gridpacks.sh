8dc3ce88e4a38c580432be224b011e109213526b
Martin <martin.flechl@cern.ch>
2018-04-04 12:07:24 +0200

    Version to run with MG5_aMC_v2.3.0.beta

---
+++ bbH-gridpacks.sh
@@ -0,0 +1,53 @@
+cmsrel CMSSW_7_1_28; cd CMSSW_7_1_28/src; cmsenv
+
+# The script makes use of a job submission module
+mkdir -p UserCode/ICHiggsTauTau/python
+wget https://raw.githubusercontent.com/ajgilbert/ICHiggsTauTau/master/python/jobs.py -O UserCode/ICHiggsTauTau/python/jobs.py
+scram b -j4
+
+git clone git@github.com:ajgilbert/MSSM-NLO.git
+cd MSSM-NLO
+
+wget https://launchpad.net/mg5amcnlo/2.0/2.2.x/+download/MG5_aMC_v2.3.0.beta.tar.gz
+tar -zxf MG5_aMC_v2.3.0.beta.tar.gz
+
+# patches needed to fix this bug: https://bugs.launchpad.net/mg5amcnlo/+bug/1483772
+patch MG5_aMC_v2_3_0_beta/vendor/CutTools/src/avh/avh_olo.f90 cuttools_avh_olo.f90.patch
+patch MG5_aMC_v2_3_0_beta/vendor/IREGI/src/oneloop/src/avh_olo_units.f90 oneloop_avh_olo_units.f90.patch
+
+# Generate dummy process directory interactively. I think this is mostly just to compile some of the common libraries we need.
+cd MG5_aMC_v2_3_0_beta/
+./bin/mg5
+generate p p > h b b~ [QCD]
+output bbH_test
+exit
+  
+cd ..
+tar -zxf bbH_4FS_yb2_modified.tar.gz
+
+# Edit any of the cards, e.g. bbH_4FS_yb2/Cards/run_card.dat for sqrt(s)
+
+# Setup directories and submit jobs
+#   -m : comma separated list of mass points to produce
+#   --cores X: tell mg to use X cores in parallel in each job.
+#              Adjust -n option in --sub-opts if running on lxbatch
+#              to request n-core machine.
+#   --req-acc: sampling grid accuracy - leave as 0.001 (CMS default)
+#   -n X: number of events to generate. Events aren't used for anything but has to be > 0
+mkdir jobs
+python launch_mg_jobs.py -m 200,250,350,400,450,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200,3500 --job-mode lxbatch --sub-opts '-q 2nd -n 4 -R "span[hosts=1]"' --task-name mgprod --step lhe --dir jobs/ --qsh 0 --cores 4 --req-acc 0.001 -n 100 #[--dry-run]
+
+
+
+
+# Once jobs are done, convert output into an official-style gridpack:
+cp -pr MG5_aMC_v2_3_0_beta MG5_aMC_v2_3_0_beta_ref
+rm -rf MG5_aMC_v2_3_0_beta_ref/bbH_4FS_yb2*
+python launch_mg_jobs.py -m 200,250,350,400,450,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200,3500 --step gridpack --dir jobs/ --qsh 0 --cores 4 -n 100 --req-acc 0.001 --gp-name SUSYGluGluToBBHToTauTau_M-
+
+
+
+#to test: new area
+cmsrel CMSSW_7_1_28; cd CMSSW_7_1_28/src; cmsenv
+tar -xavf <name>.tar.xz
+./runcmsgrid.sh 100 733 4  #<NEvents> <RandomSeed> <NumberOfCPUs>
---
+++ launch_mg_jobs.py
@@ -0,0 +1,123 @@
+from jobs import Jobs
+import argparse
+import os
+import ROOT
+import json
+import subprocess
+from math import ceil
+from math import sqrt
+from array import array
+from itertools import product
+
+job_mgr = Jobs()
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--mg-dir', default='./MG5_aMC_v2_3_0_beta')
+parser.add_argument('--base', default='./bbH_4FS_yb2')
+parser.add_argument('-n', '--nevents', default=500000, type=int)
+parser.add_argument('--mg-split', default=10000, type=int)
+parser.add_argument('--cores', default=8, type=int)
+parser.add_argument('--split', default=-1, type=int)
+parser.add_argument('--qsh', default=0, type=int)
+parser.add_argument('-m', '--mass', default='500')
+parser.add_argument('--step', default='none', choices=['none', 'lhe', 'clean', 'upload', 'gen', 'ntuple', 'xsec', 'gridpack'])
+parser.add_argument('--upload', default=None)
+parser.add_argument('--gp-name', default='SUSYGluGluToBBHToTauTau_M-')
+parser.add_argument('--download', default=None)
+parser.add_argument('--usetmp', action='store_true')
+parser.add_argument('--seed', default=0, type=int)
+parser.add_argument('--postfix', default='')
+parser.add_argument('--req-acc', default=-1.0, type=float)
+
+job_mgr.attach_job_args(parser)
+args = parser.parse_args()
+job_mgr.set_args(args)
+
+def run_cmd(arg):
+    print arg
+    os.system(arg)
+
+if args.split == -1:
+    args.split = args.nevents
+
+
+for MASS in args.mass.split(','):
+    base_name = os.path.basename(os.path.normpath(args.base))
+    massdir = '%s_%s%s' % (base_name, MASS, args.postfix)
+    if args.qsh == 1:
+        massdir += '_qshDown'
+    if args.qsh == 2:
+        massdir += '_qshUp'
+    workdir = os.path.join(args.mg_dir, massdir)
+
+    if args.step in ['lhe']:
+        if os.path.isdir(workdir):
+            print 'Dir %s already exists, skipping' % workdir
+            continue
+        else:
+            os.system('cp -r %s %s' % (args.base, workdir))
+
+        with open('%s/Cards/param_card.dat' % (workdir)) as param_file:
+            param_cfg = param_file.read()
+        param_cfg = param_cfg.replace('{MASS}', '%.5e' % float(MASS))
+        with open('%s/Cards/param_card.dat' % (workdir), "w") as outfile:
+            outfile.write(param_cfg)
+        
+        with open('%s/Cards/run_card.dat' % (workdir)) as run_file:
+            run_cfg = run_file.read()
+        run_cfg = run_cfg.replace('{TOTAL_EVENTS}', '%i' % args.nevents)
+        run_cfg = run_cfg.replace('{EVENTS_PER_JOB}', '%i' % args.mg_split)
+        run_cfg = run_cfg.replace('{REQ_ACC}', '%g' % args.req_acc)
+        run_cfg = run_cfg.replace('{SEED}', '%i' % args.seed)
+        with open('%s/Cards/run_card.dat' % (workdir), "w") as outfile:
+            outfile.write(run_cfg)
+
+        with open('%s/Cards/amcatnlo_configuration.txt' % (workdir)) as amc_file:
+            amc_cfg = amc_file.read()
+        amc_cfg = amc_cfg.replace('{MG_PATH}', '%s' % os.path.abspath(args.mg_dir))
+        amc_cfg = amc_cfg.replace('{LHAPDF_PATH}', '%s/bin/lhapdf-config' % subprocess.check_output(['scram', 'tool tag lhapdf LHAPDF_BASE']).strip())
+        with open('%s/Cards/amcatnlo_configuration.txt' % (workdir), "w") as outfile:
+            outfile.write(amc_cfg)
+        
+        with open('%s/SubProcesses/madfks_mcatnlo.inc' % (workdir)) as scale_file:
+            scale_cfg = scale_file.read()
+            frac_lo = 0.025
+            frac_hi = 0.25
+            if args.qsh == 1:
+                frac_lo /= sqrt(2.)
+                frac_hi /= sqrt(2.)
+            if args.qsh == 2:
+                frac_lo *= sqrt(2.)
+                frac_hi *= sqrt(2.)
+        scale_cfg = scale_cfg.replace('{FRAC_LO}', '%.3f' % frac_lo)
+        scale_cfg = scale_cfg.replace('{FRAC_HI}', '%.3f' % frac_hi)
+        with open('%s/SubProcesses/madfks_mcatnlo.inc' % (workdir), "w") as outfile:
+            outfile.write(scale_cfg)
+        
+        base_cmd = 'cd %s' % workdir
+        cmd = base_cmd
+        cmd += '; echo "3" | ./bin/generate_events --nb_core=%i' % args.cores
+
+        job_mgr.job_queue.append(cmd)
+    
+    if args.step in ['gridpack']:
+        gp_name = '%s%s' % (args.gp_name, MASS) 
+        run_cmd('mkdir gridpack_%s' % gp_name)
+        run_cmd('cp -a  %s gridpack_%s/process' % (workdir, gp_name))
+        run_cmd('echo "mg5_path = ../mgbasedir" >> gridpack_%s/process/Cards/amcatnlo_configuration.txt' % (gp_name))
+        run_cmd('cp -a %s_ref gridpack_%s/mgbasedir' % (args.mg_dir, gp_name))
+        run_cmd('cp runcmsgrid_NLO.sh gridpack_%s/runcmsgrid.sh' % (gp_name))
+        run_cmd('cd gridpack_%s; ../cleangridmore.sh; cd ..' % (gp_name))
+        run_cmd('cd gridpack_%s; XZ_OPT="--lzma2=preset=9,dict=512MiB" tar -cJpsf ../%s_tarball.tar.xz mgbasedir process runcmsgrid.sh; cd ..' % (gp_name, gp_name))
+
+        
+
+    if args.step in ['clean']:
+        os.system('rm -r %s/SubProcesses %s/Events/run_01/alllogs_*' % (workdir, workdir))
+
+    if args.step in ['upload'] and args.upload is not None:
+        os.system('pushd %s/Events/run_01; gunzip -c events.lhe.gz > events.lhe; gfal-copy -f events.lhe %s/%s/events.lhe; rm events.lhe; popd' % (workdir, args.upload, massdir)) 
+        #print 'pushd %s/Events/run_01; gunzip -c events.lhe.gz > events.lhe; gfal-copy -f events.lhe %s/%s/events.lhe; rm events.lhe; popd' % (workdir, args.upload, massdir) 
+        
+job_mgr.flush_queue()
+
