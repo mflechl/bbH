48dcedc6894b8e7bdb5b39d551df444c930d0001
Martin <martin.flechl@cern.ch>
2018-04-10 15:27:31 +0200

    updating branch checkout

--- bbH-gridpacks.sh
+++ bbH-gridpacks.sh
@@ -2,9 +2,8 @@
 
 cmsrel CMSSW_7_1_30; cd CMSSW_7_1_30/src; cmsenv; scram b -j 8
 
-git clone git@github.com:mflechl/bbH.git
+git clone git@github.com:mflechl/bbH.git -b MG5_v2.6
 cd bbH
-git checkout MG5_v2.6
 
 wget https://launchpad.net/mg5amcnlo/2.0/2.6.x/+download/MG5_aMC_v2.6.1.tar.gz
 tar -zxf MG5_aMC_v2.6.1.tar.gz
