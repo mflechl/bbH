78322b6788709b99e06d5d32d73afcde61bbaf0e
Martin <martin.flechl@cern.ch>
2018-04-12 13:37:23 +0200

    updating instructions

--- bbH-gridpacks.sh
+++ bbH-gridpacks.sh
@@ -31,7 +31,9 @@ tar -zxf bbH_4FS_yb2_modified.tar.gz
 mkdir jobs
 python launch_mg_jobs.py -m 200,250,350,400,450,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200,3500 --job-mode lxbatch --sub-opts '-q 2nd -n 4 -R "span[hosts=1]"' --task-name mgprod --step lhe --dir jobs/ --qsh 0 --cores 4 --req-acc 0.001 -n 100 #[--dry-run]
 
-
+#python launch_mg_jobs.py -m 200,500 --job-mode lxbatch --sub-opts '-q 2nd -n 4 -R "span[hosts=1]"' --task-name mgprod --step lhe --dir jobs/ --qsh 0 --cores 4 --req-acc 0.001 -n 100 --dry-run
+#em MG5_aMC_v2_6_1/bbH_test/Cards/amcatnlo_configuration.txt -> fastjet = /cvmfs/cms.cern.ch/slc6_amd64_gcc481//external/fastjet/3.1.0/bin/fastjet-config
+#em ./MG5_aMC_v2_6_1/bbH_test/Source/PDF/pdfwrap_lhapdf.f -> Comment out line 23, CALL SETPDFPATH(LHAPATH)
 
 
 # Once jobs are done, convert output into an official-style gridpack:
