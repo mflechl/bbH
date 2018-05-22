3da620a1c8fdd297842b2112878b243dedfb415b
Martin <martin.flechl@cern.ch>
2018-05-22 12:18:30 +0200

    update mass points

--- bbH-gridpacks.sh
+++ bbH-gridpacks.sh
@@ -30,7 +30,7 @@ tar -zxf bbH_4FS_yb2_modified.tar.gz
 #   --req-acc: sampling grid accuracy - leave as 0.001 (CMS default)
 #   -n X: number of events to generate. Events aren't used for anything but has to be > 0
 mkdir jobs
-python launch_mg_jobs.py -m 200,250,350,400,450,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200,3500 --job-mode lxbatch --sub-opts '-q 2nd -n 4 -R "span[hosts=1]"' --task-name mgprod --step lhe --dir jobs/ --qsh 0 --cores 4 --req-acc 0.001 -n 100 #[--dry-run]
+python launch_mg_jobs.py -m 80,90,100,110,120,125,130,140,160,180,200,250,350,400,450,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200,3500 --job-mode lxbatch --sub-opts '-q 2nd -n 4 -R "span[hosts=1]"' --task-name mgprod --step lhe --dir jobs/ --qsh 0 --cores 4 --req-acc 0.001 -n 100 #[--dry-run]
 
 #python launch_mg_jobs.py -m 200,500 --job-mode lxbatch --sub-opts '-q 2nd -n 4 -R "span[hosts=1]"' --task-name mgprod --step lhe --dir jobs/ --qsh 0 --cores 4 --req-acc 0.001 -n 100 --dry-run
 #em MG5_aMC_v2_6_1/bbH_test/Cards/amcatnlo_configuration.txt -> fastjet = /cvmfs/cms.cern.ch/slc6_amd64_gcc481//external/fastjet/3.1.0/bin/fastjet-config
@@ -40,7 +40,7 @@ python launch_mg_jobs.py -m 200,250,350,400,450,500,600,700,800,900,1000,1200,14
 # Once jobs are done, convert output into an official-style gridpack:
 cp -pr MG5_aMC_v2_6_1/ MG5_aMC_v2_6_1_ref
 rm -rf MG5_aMC_v2_6_1_ref/bbH_4FS_yb2*
-python launch_mg_jobs.py -m 200,250,350,400,450,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200,3500 --step gridpack --dir jobs/ --qsh 0 --cores 4 -n 100 --req-acc 0.001 --gp-name SUSYGluGluToBBHToTauTau_M-
+python launch_mg_jobs.py -m 80,90,100,110,120,125,130,140,160,180,200,250,350,400,450,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200,3500 --step gridpack --dir jobs/ --qsh 0 --cores 4 -n 100 --req-acc 0.001 --gp-name SUSYGluGluToBBHToTauTau_M-
 
 
 
