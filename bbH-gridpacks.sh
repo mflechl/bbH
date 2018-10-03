d7b367992f2cc822c743d2a00d148de08d918e75
Martin <martin.flechl@cern.ch>
2018-10-03 14:56:42 +0200

    updating gp name

--- bbH-gridpacks.sh
+++ bbH-gridpacks.sh
@@ -40,7 +40,7 @@ python launch_mg_jobs.py -m 80,90,100,110,120,125,130,140,160,180,200,250,350,40
 # Once jobs are done, convert output into an official-style gridpack:
 cp -pr MG5_aMC_v2_6_1/ MG5_aMC_v2_6_1_ref
 rm -rf MG5_aMC_v2_6_1_ref/bbH_4FS_yb2*
-python launch_mg_jobs.py -m 80,90,100,110,120,125,130,140,160,180,200,250,350,400,450,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200,3500 --step gridpack --dir jobs/ --qsh 0 --cores 4 -n 100 --req-acc 0.001 --gp-name SUSYGluGluToBBHToTauTau_M-
+python launch_mg_jobs.py -m 80,90,100,110,120,125,130,140,160,180,200,250,350,400,450,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2300,2600,2900,3200,3500 --step gridpack --dir jobs/ --qsh 0 --cores 4 -n 100 --req-acc 0.001 --gp-name SUSYGluGluToBBH_M-
 
 
 
